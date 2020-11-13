97cf809f4c53cd67aa61692de8480b434747e77a
Janis Janovskis <janis.janovskis@passivemanagement.net>
2020-11-13 12:53:54 +0200

    Clean up

--- code/web/site-install.sh
+++ code/web/site-install.sh
@@ -1,1 +1,1 @@
-drush site-install standard --db-url='mysql://drupal:f4010657f67810f399b697941b4a5433@127.0.0.1:3306/drupal' --account-name=admin --account-pass=a210c3c009814fa413f4eeb0de00a4a3 --site-name=HMP-SYSTEM --site-mail=janis@janis.test
+drush site-install minimal --db-url='mysql://drupal:f4010657f67810f399b697941b4a5433@127.0.0.1:3306/drupal' --account-name=admin --account-pass=a210c3c009814fa413f4eeb0de00a4a3 --site-name=HMP-SYSTEM --site-mail=janis@janis.test
